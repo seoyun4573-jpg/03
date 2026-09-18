4c7e931c6e2024c6ab1ddc2e2c24620d1a0edf1b
seoyun4573-jpg <seoyun4573@gmail.com>
2026-09-18 12:40:31 +0900

    03-03

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 int main(void) 
 {
     char c;
-    int i;
+    int c1;
 
-    printf("input a number :");
+    printf("input a character :");
     scanf("%c", &c);
 
-    i = c - '0';
+    //c1 = c + 1;
 
-    printf("The input number is %i\n", i);
+    printf("The next character of %c (%d) is %c(%d)\n", c,c, c+1, c+1);
 
     return 0;
 }
